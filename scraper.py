b4b7c8cd80fcaac88d8cc3019754cb9cfb2e6e87
Royce C <rchipman@uci.edu>
2024-10-30 22:19:09 -0700

    REGAE

--- scraper.py
+++ scraper.py
@@ -136,11 +136,14 @@ def is_valid(url):
                 return False
 
             # Exclude URLs with disallowed file extensions
-            if re.match(
+            if re.search(r".*(search|login|logout|api|admin|raw|static|calendar|event).*",parsed.path.lower()) or
+                re.search(r".*(page|p)/?d+", parsed.path.lower()) or
+                re.search(r".*(sessionid|sid|session)=[\w\d]{32}.*",parsed.query.lower() or
+                re.match(
                 r".*\.(css|js|bmp|gif|jpe?g|ico"
                 r"|png|tiff?|mid|mp2|mp3|mp4"
                 r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
-                r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names"
+                          r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names"
                 r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
                 r"|epub|dll|cnf|tgz|sha1"
                 r"|thmx|mso|arff|rtf|jar|csv"
